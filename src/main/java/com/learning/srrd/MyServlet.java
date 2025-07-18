a8e2b2660093bb68798490a95a1d529652c5e891
Mohan Lal <mhnuk2007@gmail.com>
2025-07-18 08:10:31 +0500

    Add servlet, index.html

---
+++ src/main/java/com/learning/srrd/MyServlet.java
@@ -0,0 +1,31 @@
+package com.learning.srrd;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/aaa")
+public class MyServlet extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String myname = request.getParameter("name1");
+		String myemail = request.getParameter("email1");
+		
+		System.out.println("Name: " + myname);
+		System.out.println("Email: " + myemail);
+		
+		PrintWriter out = response.getWriter();
+		out.println("Name: " + myname);
+		out.println("Email: " + myemail);
+		
+		
+		
+	
+		
+	}
+
+}
